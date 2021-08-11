6141bfbaefff8a4d22053109730157e20d244c5a
Raşit KAHRAMAN <34141599+kahramanrasit@users.noreply.github.com>
2021-08-11 10:59:59 +0300

    Create studentlist.h

---
+++ HandleTekniğiörnek/studentlist.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stddef.h>
+
+struct List;
+struct Student;
+typedef struct Student Student;
+typedef struct List* ListHandle;
+
+ListHandle create_list(void);
+void destoy_list(ListHandle);
+void push_front(ListHandle, const Student* p);
+void pop_front(ListHandle);
+void get_first(ListHandle, Student* p);
+void print_list(ListHandle);
+size_t get_size(ListHandle);
+int	is_empty(ListHandle);
+void make_empty(ListHandle);
